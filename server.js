567120914be48deae94febc4127bd340f7e0e2ca
vo.phi.hung <vo.phi.hung@vn-macbook-0040s-MacBook-Air.local>
2017-12-14 16:25:23 +0700

    setup heroku

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port=  process.env.PORT || 3000;
 var app = express();
+
+
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -16,9 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -50,6 +53,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up on port 3000');
 });
